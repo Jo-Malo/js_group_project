6a25857fb4a99ef8aa224984186826491cc0d0b8
Jo Malo <37541560+jo-emalo@users.noreply.github.com>
2018-07-10 21:19:06 +0100

    alphabetized cryptids in seeds

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,34 +3,24 @@ db.dropDatabase();
 
 db.cryptids.insertMany([
   {
-    name: "Nessie",
-    country: "Scotland",
+    name: "Baba Yaga",
+    country: "Russia",
     continent: "Europe",
-    habitat: ["Fresh-water"],
-    type: ["Aquatic", "Reptile"],
-    coords: [57.322857, -4.424382],
-    desc: "The Loch Ness Monster or Nessie is a cryptid of Scottish folklore, reputedly inhabiting Loch Ness in the Scottish Highlands. It is similar to other supposed lake monsters and is often described as large in size with a long neck and one or more humps protruding from the water. Popular interest and belief in the creature has varied since it was brought to worldwide attention in 1933. Evidence of its existence is anecdotal, with a few disputed photographs and sonar readings.",
-    imageSrc:"/images/nessie.jpg"
+    habitat: ["Woodland"],
+    type: ["Hybrid"],
+    coords:[61.5240 , 105.3188],
+    desc:'in Slavic Folklore (mostly Russia, but also Croatia, Serbia etc.), the Baba Yaga (Russian: Баба-яга) is a supernatural being or witch that takes the form of a hideous old woman. Baba Yaga is usually described as wearing a cloak or dress, with bright red eyes and teeth of sharpened stone or metal. It is said that she lives in a hut deep in the woods that stands on chicken legs (or one chicken leg), surrounded by a fench of bones taken from her victims, and that she flies around in a mortar wielding a pestle. There are sometimes shown to be three Baba Yagas, who are sisters. In those cases, they all have the same name.',
+    imageSrc:"/images/babayaga.jpg"
   },
   {
-    name: "Mothman",
-    country: "United States",
+    name: "Bigfoot",
+    country: "Alaska",
     continent: "North America",
-    habitat: ["Urban"],
-    type: ["Hybrid", "Flying"],
-    coords: [38.4887, -81.3482],
-    desc: "In West Virginia folklore, the Mothman is a creature reportedly seen in the Point Pleasant area from November 12, 1966, to December 15, 1967. The first newspaper report was published in the Point Pleasant Register dated November 16, 1966, titled 'Couples See Man-Sized Bird ... Creature ... Something'. The national press soon picked up the reports and helped spread the story across the United States.",
-    imageSrc:"/images/mothman.jpg"
-  },
-  {
-    name: "Chupacabra",
-    country: "Puerto Rico",
-    continent: "Central America",
-    habitat: ["Desert"],
-    type: ["Beast"],
-    coords: [18.2208, -66.5901],
-    desc: "he chupacabra or chupacabras, literally 'goat-sucker'; from chupar, 'to suck', and cabra, 'goat') is a legendary creature in the folklore of parts of the Americas, with its first purported sightings reported in Puerto Rico. The name comes from the animal's reported habit of attacking and drinking the blood of livestock, including goats.",
-    imageSrc:"/images/chupacabra.jpg"
+    habitat: ["Woodland", "Mountains"],
+    type: ["Beast", "Humanoid"],
+    coords: [64.2008, -149.4937],
+    desc: "In North American folklore, Bigfoot or Sasquatch is a hairy, upright-walking, ape-like being who reportedly dwells in the wilderness and leaves behind large footprints. Strongly associated with the Pacific Northwest (particularly Washington state and British Columbia), individuals claim to see the creature across North America. Over the years, the creature has inspired numerous commercial ventures and hoaxes.",
+    imageSrc:"/images/bigfoot.jpg"
   },
   {
     name: "Bunyip",
@@ -43,14 +33,34 @@ db.cryptids.insertMany([
     imageSrc:'/images/bunyip.jpg'
   },
   {
-    name: "Wendigo",
-    country: "Canada",
-    continent: "North America",
-    habitat: ["Woodland"],
-    type: ["Spirit", "Humanoid"],
-    coords: [70.2998 , -83.1076],
-    desc: "In Algonquian folklore, the wendigo or windigo is a mythical man eating monster or evil spirit native to the northern forests of the Atlantic Coast and Great Lakes Region of both the United States and Canada.[1] The wendigo may appear as a monster with some characteristics of a human or a spirit who has possessed a human being and made them become monstrous. It is historically associated with murder, insatiable greed, and the cultural taboos against such behaviours.",
-    imageSrc:'/images/wendigo.jpg'
+    name: "Camahueto",
+    country: "Chile",
+    continent:  "South America",
+    habitat: ["Grasslands"],
+    type: ["Beast"],
+    coords: [-35.6751, -71.5430],
+    desc: "The camahueto is a figure from Chilote mythology of the Chiloé Archipelago of Chile that has the form of a calf or bull with a small horn on its forehead, similar to a unicorn. The horn is the most valuable part of the animal, which machis use to plant small pieces in the earth from which other camahuetos will grow. When it has grown, it erupts from the earth with such force that it leaves a tremendous hole and drags down everything in its path in a rapid race to reach the sea. A machi will stalk and capture the camahueto with a lasso and tear out the horn and bandage the opening. Afterwards, she/he will use the horn for curing many kinds of illnesses.",
+    imageSrc:"/images/camahueto.jpg"
+  },
+  {
+    name: "Chupacabra",
+    country: "Puerto Rico",
+    continent: "Central America",
+    habitat: ["Desert"],
+    type: ["Beast"],
+    coords: [18.2208, -66.5901],
+    desc: "he chupacabra or chupacabras, literally 'goat-sucker'; from chupar, 'to suck', and cabra, 'goat') is a legendary creature in the folklore of parts of the Americas, with its first purported sightings reported in Puerto Rico. The name comes from the animal's reported habit of attacking and drinking the blood of livestock, including goats.",
+    imageSrc:"/images/chupacabra.jpg"
+  },
+  {
+    name:"Kongomato",
+    country:"Zambia",
+    continent:"Africa",
+    habitat:["Coastal"],
+    type:["Flying", "Reptile"],
+    coords:[-15.4067, 28.2871],
+    desc:"The kongamato ('breaker of boats') is a reported pterosaur-like creature said to have been seen by the people of and explorers in the Mwinilunga district's Jiundu swamps of Western Zambia, Angola and Congo. Suggested identities include a modern-day Rhamphorhynchus, a misidentified bird (such as the very large and peculiar saddle-billed stork), or a giant bat. No film has ever been taken, nor have any bodies been examined, leaving all of the stories to rely on large wounds and eyewitness accounts.",
+    imageSrc:"/images/kongamato.jpg"
   },
   {
     name:"Kraken",
@@ -61,6 +71,16 @@ db.cryptids.insertMany([
     coords: [70.5110, 3.5156],
     desc: "The kraken is a legendary cephalopod-like sea monster of giant size that is said to dwell off the coasts of Norway and Greenland. Authors over the years have postulated that the legend originated from sightings of giant squids that may grow to 13–15 meters (40–50 feet) in length. The sheer size and fearsome appearance attributed to the kraken have made it a common ocean-dwelling monster in various fictional works.",
     imageSrc:"/images/kraken.jpg"
+  },
+  {
+    name: "Mapinguari",
+    country: "Brazil",
+    continent: "South America",
+    habitat: ["Rainforest"],
+    type: ["Beast"],
+    coords: [3.4653, -62.2159],
+    desc: "In South American folklore, the mapinguari or mapinguary (Spanish pronunciation: [mapiŋɡwaˈɾi], Portuguese pronunciation: [mapĩɡwaˈɾi]), also known as the Isnashi [isˈnaʃi], is an entity described as resembling an ape-like or slothish creature with red fur living in the Amazon rainforests of Brazil and Bolivia. The name is usually translated as “the roaring animal” or “the fetid beast”.",
+    imageSrc:"/images/mapinguari.jpg"
   },
   {
     name: "Megalodon",
@@ -103,54 +123,24 @@ db.cryptids.insertMany([
     imageSrc:"/images/morag.jpg"
   },
   {
-    name: "Baba Yaga",
-    country: "Russia",
-    continent: "Europe",
-    habitat: ["Woodland"],
-    type: ["Hybrid"],
-    coords:[61.5240 , 105.3188],
-    desc:'in Slavic Folklore (mostly Russia, but also Croatia, Serbia etc.), the Baba Yaga (Russian: Баба-яга) is a supernatural being or witch that takes the form of a hideous old woman. Baba Yaga is usually described as wearing a cloak or dress, with bright red eyes and teeth of sharpened stone or metal. It is said that she lives in a hut deep in the woods that stands on chicken legs (or one chicken leg), surrounded by a fench of bones taken from her victims, and that she flies around in a mortar wielding a pestle. There are sometimes shown to be three Baba Yagas, who are sisters. In those cases, they all have the same name.',
-    imageSrc:"/images/babayaga.jpg"
-  },
-  {
-    name: "Yeti",
-    country: "China",
-    continent: "Asia",
-    habitat: ["Mountains"],
-    type: ["Humanoid", "Beast"],
-    coords: [30.1534 , 88.7879],
-    desc: "In the folklore of Nepal, the Yeti (/ˈjɛti/)[3] or Abominable Snowman (Nepali: हिममानव himamānav, lit. 'snow man') is an ape-like entity, taller than an average human, that is said to inhabit the Himalayan region of Nepal, Bhutan, and Tibet. The names Yeti and Meh-Teh are commonly used by the people indigenous to the region, and are part of their history and mythology. Stories of the Yeti first emerged as a facet of Western popular culture in the 19th century.",
-    imageSrc:"/images/yeti.jpg"
-  },
-  {
-    name:"Kongomato",
-    country:"Zambia",
-    continent:"Africa",
-    habitat:["Coastal"],
-    type:["Flying", "Reptile"],
-    coords:[-15.4067, 28.2871],
-    desc:"The kongamato ('breaker of boats') is a reported pterosaur-like creature said to have been seen by the people of and explorers in the Mwinilunga district's Jiundu swamps of Western Zambia, Angola and Congo. Suggested identities include a modern-day Rhamphorhynchus, a misidentified bird (such as the very large and peculiar saddle-billed stork), or a giant bat. No film has ever been taken, nor have any bodies been examined, leaving all of the stories to rely on large wounds and eyewitness accounts.",
-    imageSrc:"/images/kongamato.jpg"
-  },
-  {
-    name: "Mapinguari",
-    country: "Brazil",
-    continent: "South America",
-    habitat: ["Rainforest"],
-    type: ["Beast"],
-    coords: [3.4653, -62.2159],
-    desc: "In South American folklore, the mapinguari or mapinguary (Spanish pronunciation: [mapiŋɡwaˈɾi], Portuguese pronunciation: [mapĩɡwaˈɾi]), also known as the Isnashi [isˈnaʃi], is an entity described as resembling an ape-like or slothish creature with red fur living in the Amazon rainforests of Brazil and Bolivia. The name is usually translated as “the roaring animal” or “the fetid beast”.",
-    imageSrc:"/images/mapinguari.jpg"
-  },
-  {
-    name: "Bigfoot",
-    country: "Alaska",
+    name: "Mothman",
+    country: "United States",
     continent: "North America",
-    habitat: ["Woodland", "Mountains"],
-    type: ["Beast", "Humanoid"],
-    coords: [64.2008, -149.4937],
-    desc: "In North American folklore, Bigfoot or Sasquatch is a hairy, upright-walking, ape-like being who reportedly dwells in the wilderness and leaves behind large footprints. Strongly associated with the Pacific Northwest (particularly Washington state and British Columbia), individuals claim to see the creature across North America. Over the years, the creature has inspired numerous commercial ventures and hoaxes.",
-    imageSrc:"/images/bigfoot.jpg"
+    habitat: ["Urban"],
+    type: ["Hybrid", "Flying"],
+    coords: [38.4887, -81.3482],
+    desc: "In West Virginia folklore, the Mothman is a creature reportedly seen in the Point Pleasant area from November 12, 1966, to December 15, 1967. The first newspaper report was published in the Point Pleasant Register dated November 16, 1966, titled 'Couples See Man-Sized Bird ... Creature ... Something'. The national press soon picked up the reports and helped spread the story across the United States.",
+    imageSrc:"/images/mothman.jpg"
+  },
+  {
+    name: "Nessie",
+    country: "Scotland",
+    continent: "Europe",
+    habitat: ["Fresh-water"],
+    type: ["Aquatic", "Reptile"],
+    coords: [57.322857, -4.424382],
+    desc: "The Loch Ness Monster or Nessie is a cryptid of Scottish folklore, reputedly inhabiting Loch Ness in the Scottish Highlands. It is similar to other supposed lake monsters and is often described as large in size with a long neck and one or more humps protruding from the water. Popular interest and belief in the creature has varied since it was brought to worldwide attention in 1933. Evidence of its existence is anecdotal, with a few disputed photographs and sonar readings.",
+    imageSrc:"/images/nessie.jpg"
   },
   {
     name: "Phoenix",
@@ -163,16 +153,6 @@ db.cryptids.insertMany([
     imageSrc:"/images/phoenix.jpg"
   },
   {
-    name: "Yamata no Orochi",
-    country: "Japan",
-    continent: "Asia",
-    habitat: ["Fresh-water"],
-    type: ["Reptile"],
-    coords: [36.2048, 138.2529],
-    desc: "Yamata no Orochi legends are originally recorded in two ancient texts about Japanese mythology and history. The ca. 680 AD Kojiki transcribes this dragon name as 八岐遠呂智 and ca. 720 AD Nihongi writes it as 八岐大蛇. In both versions of the Orochi myth, the Shinto storm god Susanoo or Susa-no-Ō is expelled from Heaven for tricking his sister Amaterasu the sun-goddess.",
-    imageSrc:"/images/yamataNoOrochi.jpg"
-  },
-  {
     name: "Troll",
     country: "Norway",
     continent: "Europe",
@@ -183,16 +163,6 @@ db.cryptids.insertMany([
     imageSrc: "/images/troll.jpg"
   },
   {
-    name: "Camahueto",
-    country: "Chile",
-    continent:  "South America",
-    habitat: ["Grasslands"],
-    type: ["Beast"],
-    coords: [-35.6751, -71.5430],
-    desc: "The camahueto is a figure from Chilote mythology of the Chiloé Archipelago of Chile that has the form of a calf or bull with a small horn on its forehead, similar to a unicorn. The horn is the most valuable part of the animal, which machis use to plant small pieces in the earth from which other camahuetos will grow. When it has grown, it erupts from the earth with such force that it leaves a tremendous hole and drags down everything in its path in a rapid race to reach the sea. A machi will stalk and capture the camahueto with a lasso and tear out the horn and bandage the opening. Afterwards, she/he will use the horn for curing many kinds of illnesses.",
-    imageSrc:"/images/camahueto.jpg"
-  },
-  {
     name: "Vorota Beast",
     country: "Russia",
     continent: "Europe",
@@ -201,5 +171,35 @@ db.cryptids.insertMany([
     coords: [61.0178, 99.1966],
     desc: "The Vorota beast is a reported creature seen in Lake Vorota in Eastern Siberia, one of several isolated lakes on the Sordongnokh tablelands which are said to contain monsters. Russian geologist Viktor A. Tverdokhlebov and Boris Bashkatov were with a group doing geological studies near the lake in July 1953. While visiting the lake, they reported seeing a barrel-sized hump moving across the lake. As it got closer, a head appeared. Its head was around six feet wide, and the body between 30 and 40 feet long. On its hump was a large fin. It slapped the water, then dove under the water.",
     imageSrc: "/images/vorotabeast.png"
+  },
+  {
+    name: "Wendigo",
+    country: "Canada",
+    continent: "North America",
+    habitat: ["Woodland"],
+    type: ["Spirit", "Humanoid"],
+    coords: [70.2998 , -83.1076],
+    desc: "In Algonquian folklore, the wendigo or windigo is a mythical man eating monster or evil spirit native to the northern forests of the Atlantic Coast and Great Lakes Region of both the United States and Canada.[1] The wendigo may appear as a monster with some characteristics of a human or a spirit who has possessed a human being and made them become monstrous. It is historically associated with murder, insatiable greed, and the cultural taboos against such behaviours.",
+    imageSrc:'/images/wendigo.jpg'
+  },
+  {
+    name: "Yamata no Orochi",
+    country: "Japan",
+    continent: "Asia",
+    habitat: ["Fresh-water"],
+    type: ["Reptile"],
+    coords: [36.2048, 138.2529],
+    desc: "Yamata no Orochi legends are originally recorded in two ancient texts about Japanese mythology and history. The ca. 680 AD Kojiki transcribes this dragon name as 八岐遠呂智 and ca. 720 AD Nihongi writes it as 八岐大蛇. In both versions of the Orochi myth, the Shinto storm god Susanoo or Susa-no-Ō is expelled from Heaven for tricking his sister Amaterasu the sun-goddess.",
+    imageSrc:"/images/yamataNoOrochi.jpg"
+  },
+  {
+    name: "Yeti",
+    country: "China",
+    continent: "Asia",
+    habitat: ["Mountains"],
+    type: ["Humanoid", "Beast"],
+    coords: [30.1534 , 88.7879],
+    desc: "In the folklore of Nepal, the Yeti (/ˈjɛti/)[3] or Abominable Snowman (Nepali: हिममानव himamānav, lit. 'snow man') is an ape-like entity, taller than an average human, that is said to inhabit the Himalayan region of Nepal, Bhutan, and Tibet. The names Yeti and Meh-Teh are commonly used by the people indigenous to the region, and are part of their history and mythology. Stories of the Yeti first emerged as a facet of Western popular culture in the 19th century.",
+    imageSrc:"/images/yeti.jpg"
   }
 ]);
