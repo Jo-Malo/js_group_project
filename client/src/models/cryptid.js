bc4d0aad62ce35226297395226e65ea49bab4f89
Joe Stafford <joestafford1986@gmail.com>
2018-07-10 13:40:27 +0100

    added sorting method

--- client/src/models/cryptid.js
+++ client/src/models/cryptid.js
@@ -4,22 +4,29 @@ const Request = require('../helpers/request.js');
 
 const Cryptid = function(url) {
   this.url = url;
+  this.cryptids = [];
 };
 
 Cryptid.prototype.bindEvents = function () {
   PubSub.subscribe('SelectView:select-change', (evt) => {
     const selectedContinent = evt.detail;
-
-
-    
+    this.getCryptidData();
+    PubSub.subscribe('Cryptid:data-set', (evt) => {
+      console.log(evt.detail);
+      const filteredData = this.filterDataByContinent(evt.detail, selectedContinent);
+      console.log(filteredData);
+    })
+    console.log(this.cryptids);
+    // console.log(this.filterDataByContinent(this.cryptids, selectedContinent));
   })
+
+
 
 
   PubSub.subscribe('DirectoryView:li-clicked', (evt) => {
     const request = new Request(this.url + `/${evt.detail}`)
     request.get()
      .then((data) => {
-       console.log(data);
        PubSub.publish('Cryptid:data-selected', data);
      });
   });
@@ -30,8 +37,25 @@ Cryptid.prototype.getData = function() {
    request.get()
     .then((data) => {
       PubSub.publish('Cryptid:data-loaded', data);
-    });
- };
+  });
+};
+
+Cryptid.prototype.getCryptidData = function() {
+   const request = new Request(this.url)
+   request.get()
+    .then((data) => {
+      this.cryptids = [];
+      this.cryptids.push(data);
+      PubSub.publish('Cryptid:data-set', data);
+      console.log(this.cryptids);
+  });
+};
+
+Cryptid.prototype.filterDataByContinent = function (data, continent) {
+  let filteredData = [];
+  filteredData.push(data.filter(cryptid => cryptid.continent === continent));
+  return filteredData;
+};
 
 Cryptid.prototype.showCryptidOnSidebar = function () {
   PubSub.subscribe('MapView: Pin-Selected', (evt)=>{
